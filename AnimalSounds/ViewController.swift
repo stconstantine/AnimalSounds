888e2e6cb2bc17d2e5d1b8edf3befa7611c46995
strkto <67833385+strkto@users.noreply.github.com>
2020-07-08 19:42:18 +0300

    first commit, sound don't work :(

---
+++ AnimalSounds/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  AnimalSounds
+//
+//  Created by Константин Стародубцев on 08.07.2020.
+//  Copyright © 2020 Константин Стародубцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    let mooSound = SimpleSound(named: "moo")
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+   
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
+    @IBAction func cowButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Муу!"
+        mooSound.play()
+    }
+    @IBAction func dogButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Гав!"
+        woofSound.play()
+    }
+    @IBAction func catButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Мур!"
+        meowSound.play()
+    }
+    
+}
+
